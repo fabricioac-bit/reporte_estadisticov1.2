d887406cd71ddef1ea47be30199d8a51166c5830
fabricioac01 <2201010005@undc.edu.pe>
2026-06-03 00:03:52 -0500

    submodulo de CE/menu lateral

---
+++ src/app/dashboard/financiamiento/page.tsx
@@ -0,0 +1,7 @@
+export default function PruebasPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold text-slate-800">Módulo en Desarrollo</h1>
+    </div>
+  );
+}
